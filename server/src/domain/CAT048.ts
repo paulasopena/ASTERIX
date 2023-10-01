7338285f1ddd7820bfb375c4362fe8de93917383
Itziar Mensa <itziar.mensa@estudiantat.upc.edu>
2023-10-01 21:37:13 +0200

    feat: decoder in progress

--- server/src/domain/CAT048.ts
+++ server/src/domain/CAT048.ts
@@ -28,105 +28,159 @@ export class CAT048 {
 
     async decodeMessages() {
 
-        const length = this.messages.readUInt16BE(1);
+        const length = this.messages.readUInt16BE(1); // Comencem a llegir 16 desde la posició 1 (ens saltem la CAT)
         const realLength = this.messages.length;
         assert.strictEqual(length, realLength, 'Length mismatch.');
 
         var i = 0;
         var moreFSPEC = true;
-        var counter = 4;
 
-        while (i < 4) {
+        var numFSPEC = 0;
+
+        while (i <= 4) { // Analitzem el FSPEC
             if ( moreFSPEC ) {
-                console.log("SPECK")
                 var currentByte  = this.messages.readUInt8(i + 3);
                 const binaryArray = currentByte.toString(2).padStart(8, '0').split('').reverse();       // [7,6,5,4,3,2,1,0]
                 
-
-                switch (i) {
-                    case 0:
-                        if (binaryArray[7] === '1') {
-                            this.setDataSourceIdentifier(this.messages.subarray(counter, counter + 2))
-                            const array = this.messages.subarray(counter, counter + 2)
-                            console.log(array.readUInt8(0) + "andd" + array.readUInt8(1))
-                        }
-                        if (binaryArray[6] === '1') {
-
-                        }
-                        if (binaryArray[5] === '1') {
- 
-                        }
-                        if (binaryArray[4] === '1') {
-    
-                        }
-                        if (binaryArray[3] === '1') {
-
-                        }
-                        if (binaryArray[2] === '1') {
-
-                        }
-                        if (binaryArray[1] === '1') {
-
-                        }
-                        break;
-                    case 1:
-                        if (binaryArray[7] === '1') {
-                            
-                        }
-                        if (binaryArray[6] === '1') {
-
-                        }
-                        if (binaryArray[5] === '1') {
-
-                        }
-                        if (binaryArray[4] === '1') {
-
-                        }
-                        if (binaryArray[3] === '1') {
-
-                        }
-                        if (binaryArray[2] === '1') {
-
-                        }
-                        if (binaryArray[1] === '1') {
-
-                        }
-                        break;
-                    case 2:
-                        if (binaryArray[7] === '1') {
-                            
-                        }
-                        if (binaryArray[6] === '1') {
-
-                        }
-                        if (binaryArray[5] === '1') {
-
-                        }
-                        if (binaryArray[4] === '1') {
-
-                        }
-                        if (binaryArray[3] === '1') {
-
-                        }
-                        if (binaryArray[2] === '1') {
-
-                        }
-                        if (binaryArray[1] === '1') {
-
-                        }
-                        break;
-                }
-                
-                if (binaryArray[0] === '0') {
+                if (binaryArray[0] === '1') {
+                    numFSPEC += 1;
+                } else {
+                    numFSPEC += 1; // Aquí també per saber quants 'blocs' de data items també, el últim FSPEC será false però tindrem 3 blocs (bytes) 
                     moreFSPEC = false;
-                }
+                }               
             }
             i = i + 1;
         }
+
+        console.log('Number FSPEC: ' + numFSPEC);
+
+        var j = 0;
+
+        while (j <= numFSPEC) {
+            var currentByte  = this.messages.readUInt8(j + 3);
+            const binaryArray = currentByte.toString(2).padStart(8, '0').split('').reverse();       // [7,6,5,4,3,2,1,0]
+
+            switch (j) {
+                case 0:
+
+                    /*  
+                        Data Item I048/010, Data Source Identifier
+                        Definition: Identification of the radar station from which the data is received.
+                        Format: Two-octet fixed length Data Item.
+                    */
+                    if (binaryArray[7] === '1') {
+                        var octet1  = this.messages.readUInt8(numFSPEC + 3).toString(2).padStart(8, '0').split('').reverse();
+                        var octet2  = this.messages.readUInt8(numFSPEC + 4).toString(2).padStart(8, '0').split('').reverse();
+                        const array = [...octet1, ...octet2];
+                        console.log('Data Source Identifier (bits): ' + array);
+                        console.log('Data Source Identifier (decimal): ' + this.messages.readUInt8(numFSPEC + 3) + ' ' + this.messages.readUInt8(numFSPEC + 4));
+                        this.setDataSourceIdentifier(array);
+                    } else {
+                        console.log('Data Source Identifier: null');
+                    }
+
+                    /*  
+                        Data Item I048/140, Time of Day
+                        Definition: Absolute time stamping expressed as Co-ordinated Universal Time (UTC).
+                        Format: Three-octet fixed length Data Item. 
+                    */
+                    if (binaryArray[6] === '1' && binaryArray[7] === '0') {
+                        var octet1  = this.messages.readUInt8(numFSPEC + 3).toString(2).padStart(8, '0').split('').reverse();
+                        var octet2  = this.messages.readUInt8(numFSPEC + 4).toString(2).padStart(8, '0').split('').reverse();
+                        var octet3  = this.messages.readUInt8(numFSPEC + 5).toString(2).padStart(8, '0').split('').reverse();
+                        const array = [...octet1, ...octet2, octet3];
+                        console.log('Time of Day (bits): ' + array);
+                        console.log('Time of Day (decimal): ' + this.messages.readUInt8(numFSPEC + 3) + ' ' + this.messages.readUInt8(numFSPEC + 4) + ' ' + this.messages.readUInt8(numFSPEC + 5));
+
+                    } else if (binaryArray[6] === '1' && binaryArray[7] === '1') {
+                        var octet1  = this.messages.readUInt8(numFSPEC + 5).toString(2).padStart(8, '0').split('').reverse();
+                        var octet2  = this.messages.readUInt8(numFSPEC + 6).toString(2).padStart(8, '0').split('').reverse();
+                        var octet3  = this.messages.readUInt8(numFSPEC + 7).toString(2).padStart(8, '0').split('').reverse();
+                        const array = [...octet1, ...octet2, ...octet3];
+                        console.log('Time of Day (bits): ' + array);
+                        console.log('Time of Day (decimal): ' + this.messages.readUInt8(numFSPEC + 5) + ' ' + this.messages.readUInt8(numFSPEC + 6) + ' ' + this.messages.readUInt8(numFSPEC + 7));
+                    } else {
+                        console.log('Time of Day: null');
+                    }
+
+                    /*
+                        Data Item I048/020, Target Report Descriptor
+                        Definition: Type and properties of the target report.
+                        Format: Variable length Data Item comprising a first part of one-octet, followed by one-octet extents as necessary.
+                    */
+                    if (binaryArray[5] === '1') {
+
+                    }
+                    if (binaryArray[4] === '1') {
+
+                    }
+                    if (binaryArray[3] === '1') {
+
+                    }
+                    if (binaryArray[2] === '1') {
+
+                    }
+                    if (binaryArray[1] === '1') {
+
+                    }
+                    break;
+                case 1:
+                    if (binaryArray[7] === '1') {
+                        
+                    }
+                    if (binaryArray[6] === '1') {
+
+                    }
+                    if (binaryArray[5] === '1') {
+
+                    }
+                    if (binaryArray[4] === '1') {
+
+                    }
+                    if (binaryArray[3] === '1') {
+
+                    }
+                    if (binaryArray[2] === '1') {
+
+                    }
+                    if (binaryArray[1] === '1') {
+
+                    }
+                    break;
+                case 2:
+                    if (binaryArray[7] === '1') {
+                        
+                    }
+                    if (binaryArray[6] === '1') {
+
+                    }
+                    if (binaryArray[5] === '1') {
+
+                    }
+                    if (binaryArray[4] === '1') {
+
+                    }
+                    if (binaryArray[3] === '1') {
+
+                    }
+                    if (binaryArray[2] === '1') {
+
+                    }
+                    if (binaryArray[1] === '1') {
+
+                    }
+                    break;
+            }
+
+            j += 1;
+        }
     }
 
-    async setDataSourceIdentifier(buffer: Buffer) {
-
+    async setDataSourceIdentifier(buffer: string[]) {
+        var SAC = buffer.slice(0, 7);
+        var SIC = buffer.slice(8, 15);
+        console.log('SAC: ' + SAC);
+        console.log('SIC: ' + SIC);
     }
 
     async setTargetReportDescriptor(buffer: Buffer) {
